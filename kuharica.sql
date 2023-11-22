15b7e6e017df05c1e9e6ef5f22e47e44cca27d76
NatalijaGlavas <natalija-glavas@hotmail.com>
2023-11-22 11:52:51 +0100

    dodala kuharicu

---
+++ kuharica.sql
@@ -0,0 +1,26 @@
+
+--create database kuharica;
+
+--use kuharica;
+
+--create table autor(
+--sifra int not null primary key identity(1,1),
+--ime varchar(20) not null,
+--prezime varchar(20) not null,
+--email varchar(100) not null,
+--mjesto varchar(50) not null,
+--drzava varchar(50) not null
+--);
+
+
+--create table recepti(
+--sifra int not null primary key identity(1,1),
+--naziv varchar(50) not null,
+--imeautora varchar(20) not null,
+--opis varchar not null
+--);
+
+
+alter table 
+
+
