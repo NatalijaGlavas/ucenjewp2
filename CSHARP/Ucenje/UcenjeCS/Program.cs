aaf43423d73f69731e9be707b08f603a90b0031a
NatalijaGlavas <natalija-glavas@hotmail.com>
2024-01-01 20:02:53 +0100

    WebAPI projekt

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+/*
+ 
+Komentar kroz više linija - ne preporuča se
+ 
+
+Console.WriteLine(7);
+Console.WriteLine(true);
+Console.Write("Prvi");
+Console.Write("Drugi");
+Console.WriteLine(3.14);
+*/
+
+//varijabla je prostor u memoriji
+//Console.Write("Unesi ime"  );
+//string Ime = Console.ReadLine();
+
+//Console.WriteLine("Unjeli ste " + Ime );
+
+Console.Write("Unesi visinu u centimetrima:");
+int Visina = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Visoki ste " + (float)Visina / 100 + "metara");
+
+
+
+
+
