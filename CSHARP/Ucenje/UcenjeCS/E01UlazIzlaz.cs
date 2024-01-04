44e7418803ce69bb16ad1f24b1c18e01e0aaaaec
NatalijaGlavas <natalija-glavas@hotmail.com>
2024-01-04 12:24:20 +0100

    Varijable tipovi podataka i operatori

---
+++ CSHARP/Ucenje/UcenjeCS/E01UlazIzlaz.cs
@@ -0,0 +1,76 @@
+﻿
+namespace UcenjeCS
+{
+    internal class E01UlazIzlaz
+    {
+
+        public static void Izvedi()
+        {
+
+
+            /*
+
+            Komentar kroz više linija - ne preporuča se
+
+            //ispisi različitih tipova podataka
+            Console.WriteLine(7); // int
+            Console.WriteLine(true); // bool
+            Console.Write("Prvi"); // string
+            Console.Write("Drugi"); // string
+            Console.WriteLine(3.14); // float
+            */
+
+            //varijabla je prostor u memoriji
+            //Console.Write("Unesi ime"  );
+            //string Ime = Console.ReadLine();
+
+            //Console.WriteLine("Unjeli ste " + Ime );
+
+            //TIPIČAN PROGRAM IMA
+
+            // ulaz
+            Console.Write("Unesi visinu u centimetrima:");
+            int Visina = int.Parse(Console.ReadLine());
+
+
+            // algoritam
+            float VisinaUMetrima = (float)Visina / 100;
+
+
+            //izlaz
+            Console.WriteLine("Visoki ste " + (float)Visina / 100 + "metara");
+
+
+            // Učitati decimalni broj i ispisati ga
+            float DecimalniBroj = float.Parse(Console.ReadLine());
+
+
+            // Riješavati https:// adventofcode.com/
+            // Program unosi dužinu i širinu prostorije
+            //Program ispisuje površinu prostorije
+
+
+            Console.Write(" Unesi dužinu prostorije : ");
+            float Duzina = float.Parse(Console.ReadLine());
+
+            Console.Write(" Unesi širinu prostorije: ");
+            float Sirina = float.Parse(Console.ReadLine());
+
+            var Povrsina = Duzina * Sirina;
+
+            Console.WriteLine(Povrsina);
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ CSHARP/Ucenje/UcenjeCS/E02VarijableTipoviPodatakaOperatori.cs
@@ -0,0 +1,97 @@
+﻿
+namespace UcenjeCS
+{
+    internal class E02VarijableTipoviPodatakaOperatori
+    {
+
+        public static void Izvedi()
+        {
+            int Varijabla = 3;
+
+            Console.WriteLine(Varijabla);
+
+            int i=1, j=0, k;
+
+            Console.WriteLine("{0} - {1}", i, j);
+
+            bool Istina = i == 1;
+
+            Console.WriteLine(Istina);
+
+            double Broj = 4.89;
+            decimal VeciBroj = 2.99M;
+
+            //brojevi se nalaze na brojevnoj kružnici
+            Console.WriteLine(int.MaxValue);
+            int Mb = int.MaxValue;
+            Console.WriteLine(Mb + 1);
+
+            i = 3; j = 2;  k = 1;
+
+            Console.WriteLine(i+j);
+            Console.WriteLine(i-j);
+            Console.WriteLine(i*j); //množenje
+            Console.WriteLine(i/j); //djelenje - djelenje daje cijeli broj
+            Console.WriteLine((float) i/j);//djelenje s decimalnim rezultatom
+
+            //za dvoznamenkasti broj ispiši prvu znamenku
+            int db = 56;
+            Console.WriteLine(db/10);
+
+            bool uvjet = i > j;
+            uvjet = i >= j;
+            uvjet = i <= j;
+            uvjet = i > j;
+            uvjet = i == j; //provjera jednakosti
+            uvjet = i != j; //provjera ne jednakosti
+
+
+            //operator modulo
+            //ostatak nakon cjelobrojnog djelenja
+            int ostatak = 9 % 2;
+
+            //za dvoznamekasti broj ispiši vrijednost jedinice
+            Console.WriteLine(52 % 10);
+
+            i = 1;
+            Console.WriteLine(i); //1
+            i = i + 1;  //uvećavam za 1
+            Console.WriteLine(i); //2
+            i += 1; //uvećavam za 1
+            Console.WriteLine(i); //3
+            i++; //uvećavam za 1
+            Console.WriteLine(i); //4
+
+            //specifičnosti incrementa ++
+            // i++ prvo koristi trenutnu vrijednost pa onda uveća
+            Console.WriteLine(i++); //4
+            Console.WriteLine(i); //5
+            // ++i prvo uveća pa onda koristi
+            Console.WriteLine(++i); //6
+            Console.WriteLine(i); //6
+
+            // SVE VRIJEDI ISTOVJETNO i za dekrement (--)
+
+            int t = 1, l = 2;
+            t = ++t - l; // t = 0, l=2
+            Console.WriteLine(" t = ++t - l =>  t={0}, l={1}", t , l);
+
+            l -= t - l; // l =2, t = 0
+            Console.WriteLine("l -= t - l => t={0}, l={1}", t, l);
+
+            Console.WriteLine(++t - --l); // 1 -1 = 0
+            Console.WriteLine("++t  - --l => t={0}, l={1}", t, l);
+
+
+            //Ovo vježbati doma na način
+            //Prvo si postavi zadatak pa ga nakon 15 minuta riješiti
+
+
+
+   
+
+
+
+        }
+    }
+}
