ad83d77715a45d76cae0102762777001fe129e7c
NatalijaGlavas <natalija-glavas@hotmail.com>
2024-01-11 12:28:40 +0100

    WebAPI zadatak 1

--- CSHARP/Ucenje/WebAPI/Controllers/E02VarijableTipoviPodatakaOperatori.cs
+++ CSHARP/Ucenje/WebAPI/Controllers/E02VarijableTipoviPodatakaOperatori.cs
@@ -4,12 +4,11 @@ namespace WebAPI.Controllers
 {
     [ApiController]
     [Route("E02")]
-    public class E02VarijableTipovi_PodatakaOperatori :ControllerBase
+    public class E02VarijableTipoviPodatakaOperatori:ControllerBase
     {
         [HttpGet]
         [Route("zad1")]
-        
-        public int zad1()
+        public int Zad1()
         {
             //vratite najmanji broj
             return int.MinValue;
