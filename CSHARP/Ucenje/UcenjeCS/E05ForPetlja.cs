f508049439ea4f0155d4284967f1885b6600a469
NatalijaGlavas <natalija-glavas@hotmail.com>
2024-01-15 01:15:02 +0100

    For petlja

--- CSHARP/Ucenje/UcenjeCS/E05ForPetlja.cs
+++ CSHARP/Ucenje/UcenjeCS/E05ForPetlja.cs
@@ -1,4 +1,7 @@
-﻿namespace UcenjeCS
+﻿using System;
+using System.Threading.Channels;
+
+namespace UcenjeCS
 {
     internal class E05ForPetlja
     {
@@ -75,12 +78,81 @@
 
 
 
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j=0; j<10; j++)
+                {
+                    Console.WriteLine((i+1) * (j+1) + "  ");
+                }
+                Console.WriteLine();
+            }
+            // DZ popraviti formatiranje
+
+
+            // Petlja se može preskočiti
+
+            for(int i = 0; i<10; i++)
+            {
+                if(i%3==0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(i);
+            }
+
+            // petlja se može nasilno prekinuta
+            for(int i = 0; i < 10; i++)
+            {
+                if(i == 3)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+
+
+            // beskonačna petlja
+            for(; ; )
+            {
+                Console.Write(new Random().Next(10,100) + " ");
+                Console.Write(new Random().Next(10, 100) + " ");
+                Console.Write(new Random().Next(10, 100) + " ");
+                Thread.Sleep(50);
+                break;
         }
 
 
+            // za unesen broj između 1 i 10
+            // ispiši taj broj na kvadrat
+
+            int broj;
+
+            for(; ; )
+            {
+                Console.Write("Unesi broj između 1 i 10:");
+                broj = int.Parse(Console.ReadLine());
+                if(broj>=1 && broj <= 10)
+                {
+                    break;
+                }
+                Console.WriteLine("Dragi korisniče, krvi unos");
+            }
+
+            Console.WriteLine(broj*broj);
 
 
 
+
+
+
+
+
+
+
+            // kraj metode
+        }
 
 
     }
