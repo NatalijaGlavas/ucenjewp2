8241b4e22f0e29731416199ded2f4a97aa2e670a
NatalijaGlavas <natalija-glavas@hotmail.com>
2024-02-13 19:40:28 +0100

    EdunovaAPP preduvjeti

---
+++ CSHARP/Ucenje/EdunovaAPP/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// prilagodba za dokumentaciju, čitati https://medium.com/geekculture/customizing-swagger-in-asp-net-core-5-2-c98d03cbe52
+builder.Services.AddSwaggerGen(sgo =>
+{ // sgo je istanca klase SwaggerGenOptions
+  // čitati https://devintxcontent.blob.core.windows.net/showcontent/Speaker%20Presentations%20Fa11%202017/Web%20API%20Practices.pdf
+    var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Edunova API",
+        Version = "v1",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Email = "tjakopec@gmail.com",
+            Name = "Tomislav Jakopec"
+        },
+        Description = "Ovo je dokumentacija za Edunova API",
+        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+        {
+            Name = "Edukacijska licenca"
+        }
+    };
+    sgo.SwaggerDoc("v1", o);
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
